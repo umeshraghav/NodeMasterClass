888aac5e94ce00683929642299c06d0cf9bcf977
Umesh Raghav <umeshraghav55@gmail.com>
2021-05-24 15:22:20 +0530

    Parse the http method

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 
 //dependencies 
 const http = require('http');
-
+const url = require('url')
 
 const server = http.createServer(function(req,res){
+
+        // get the url
+        const parsedUrl = url.parse(req.url, true)
+
+        //get the parthname
+        const pathName = parsedUrl.pathname;
+
+        //get the http method 
+        const method = req.method.toLowerCase();
+
+
+        console.log(method)
+
     res.end("Http Server");
+
+    //Get the URL and parse it
+
 })
+
 
 
 //listen to the server on porn 3000
